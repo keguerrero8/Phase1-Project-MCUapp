fc72aa29e1d236e601ec6559c0fe1fcb75e831d2
Kevin Guerrero <guerrk83z@gmail.com>
2021-12-07 19:32:30 -0500

    add JS functionality for generating lists of interests and displaying movie info

--- index.js
+++ index.js
@@ -1,86 +1,197 @@
-//console.log('%c HI', 'color: firebrick')
+//1. At page refresh, show featured movie (or most recent release) -- DONE
+//2. Click on any of the left side pane lists, to view a populated list of movies on the right, 
+//and the first of that list in the center -- DONE
+//3. When a movie is selected from the right pane, it should populate at the center pane
+//4. Ability for person to click on add to My list and keep record of those movies
+//5. Click on View My list from left pane and generate the list of movies you have added
+//6. Implement search bar functionality
+document.addEventListener("DOMContentLoaded", e => {
+    initialize()
+    topTenLoadList()
+    releaseDateLoadList()
+    chronologicalLoadList()
+})
 
-//Challenge 1
-function addImages () {
+//initialize some variables
+let today = new Date()
+let todayDate = new Date(today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate())
+
+
+
+// let anotherDate = new Date("2020-05-02")
+
+// if (todayDate.getTime() > anotherDate.getTime()) {
+//     console.log(todayDate.getTime() - anotherDate.getTime())
+// }
+
+//this will load the upcoming movie to be released at the center pane of the app upon reload
+function initialize () {
     fetch("https://mcuapi.herokuapp.com/api/v1/movies")
     .then(data => data.json())
     .then(json => {
-        // create variable for the div
-        // take one element from json, and append it to the div
-        const img = document.querySelector("#movie-image")
-        const h2 = document.querySelector("h2")
-        const text = document.querySelector("#movie-description")
-        const ulMovies = document.querySelector("#movie-items")
-        ulMovies.innerHTML = ""
-        h2.textContent = json.data[1].title
-        img.src = json.data[1].cover_url
-        text.textContent = json.data[1].overview
-
-        for (i=0; i < json.data.length; i++) { //index 21, Guardians of the galaxy 3 is the problem, does not have a url to cover
-            if (json.data[i].box_office !== "0") {
-                const li = document.createElement("li")
-                const hr = document.createElement("hr")
-                li.textContent = json.data[i].title
-                li.className = "listcategories"
-                ulMovies.appendChild(li)
-                ulMovies.appendChild(hr)
-                // console.log(json.data[i].title)
-                // console.log(json.data[i].chronology) 
+        const startTime = new Date(json.data[1].release_date)
+        let minTime = startTime.getTime()
+        let movieToDisplay
+        for (let movie of json.data) {
+            const movieDate = new Date(movie.release_date)
+            const timeDiff = Math.abs(todayDate.getTime() - movieDate.getTime())
+            if (timeDiff < minTime && movie.box_office === "0") {
+                minTime = timeDiff
+                movieToDisplay = movie
             }
         }
+        displayMovieCenter(movieToDisplay)
+    })
+}
 
-        // json.data.forEach(el => {
-        //     const img = document.createElement("img")
-        //     img.src = el.cover_url
-        //     img.style.height = "600px"
+function topTenLoadList () {
+    const list = document.querySelector("#topTen")
+    list.addEventListener("click", event => {
+        fetch("https://mcuapi.herokuapp.com/api/v1/movies")
+        .then(data => data.json())
+        .then(json => {
+            let topTenList = []
+            let maxVal = 0
+            let movieToAdd
+            for (let i = 0; i < 10; i++) {
+                for (let i = 0; i < json.data.length; i++) {
+                    if (i===0) {
+                        maxVal = json.data[i].box_office
+                        movieToAdd = json.data[i]
+                    }
+                    else if (parseInt(json.data[i].box_office) > parseInt(maxVal)) {
+                        maxVal = json.data[i].box_office
+                        movieToAdd = json.data[i]             
+                    }
+                }
+                topTenList.push(movieToAdd)
+                indexToRemove = json.data.indexOf(movieToAdd)
+                json.data.splice(indexToRemove, 1)
+            }
+            addMovieList(topTenList, "Top 10 Rated")
+        })
+    })
+}
 
-        //     //h1.textContent = json.data[1].title 
-    
-        //     div.appendChild(img)         
+function releaseDateLoadList () {
+    const list = document.querySelector("#releaseDate")
+    list.addEventListener("click", event => {
+        fetch("https://mcuapi.herokuapp.com/api/v1/movies")
+        .then(data => data.json())
+        .then(json => {
+            let releaseDateList = []
+            let minVal = 0
+            let movieToAdd
+            const movieCount = json.data.length
 
-        // })
-        // const img = document.createElement("img")
-        // img.src = json.data[1].cover_url
-        // img.style.height = "600px"
-        // h1.textContent = json.data[1].title 
+            for (let i = 0; i < movieCount; i++) {
+                for (let i = 0; i < json.data.length; i++) {
+                    const currentRelease = new Date(json.data[i].release_date)
+                    if (i===0) {
+                        minVal = new Date(json.data[i].release_date)
+                        movieToAdd = json.data[i]
+                    }
+                    else if (json.data[i].release_date !== null &&  currentRelease < minVal) {
+                        minVal = currentRelease
+                        movieToAdd = json.data[i]             
+                    }
+                }
+                //only add the movie to list if it has been released
+                if (movieToAdd.release_date !== null) {
+                    releaseDateList.push(movieToAdd)
+                    indexToRemove = json.data.indexOf(movieToAdd)
+                    json.data.splice(indexToRemove, 1)
+                }
+            }
+            addMovieList(releaseDateList, "Release date")
+        })
+    })
+}
 
-        // div.appendChild(img)
+function chronologicalLoadList () {
+    const list = document.querySelector("#chronologically")
+    list.addEventListener("click", event => {
+        fetch("https://mcuapi.herokuapp.com/api/v1/movies")
+        .then(data => data.json())
+        .then(json => {
+            let chronologicalList = []
+            let movieToAdd
+            let chronologyCount = 1
 
-
-        // json.message.forEach((url) => {
-        //     const img = document.createElement("img")
-        //     img.src = url
-        //     div.appendChild(img)
-        // }      
-        // )
-
+            for (let i = 0; i < json.data.length; i++) {
+                movieToAdd = null
+                for (let i = 0; i < json.data.length; i++) {
+                    if (json.data[i].chronology === chronologyCount) {
+                        movieToAdd = json.data[i]
+                        break
+                    }
+                }
+                chronologyCount++
+                if (movieToAdd !== null) {
+                    chronologicalList.push(movieToAdd)
+                }
+                else {
+                    break
+                }
+            }
+            addMovieList(chronologicalList, "MCU Timeline")
+        })
     })
 }
 
 
-document.addEventListener("DOMContentLoaded", addImages)
+function displayMovieCenter (movieObject) {
+    //save elements to variables
+    const img = document.querySelector("#movie-image")
+    const h2 = document.querySelector("h2")
+    const text = document.querySelector("#movie-description")
+    const releaseDate = document.querySelector("#movie-release")
+    const boxOffice = document.querySelector("#movie-box-office")
+    //save data to elements to display
+    h2.textContent = movieObject.title
+    img.src = movieObject.cover_url
+    text.textContent = movieObject.overview
+    releaseDate.textContent =movieObject.release_date
+    boxOffice.textContent =movieObject.box_office
 
-//Challenge 2
+}
 
-// function addDogBreeds () {
-//     fetch("https://dog.ceo/api/breeds/list/all")
-//     .then(data => data.json())
-//     .then(json => {
-//         const ul = document.querySelector("#dog-breeds")
-//         for (let breed in json.message) {
-//             const li = document.createElement("li")
-//             li.textContent = breed
-//             ul.appendChild(li)
-//             li.addEventListener("click", changeColor)
-//         }
-//     })
+function addMovieList (list, listString) {
+    displayMovieCenter(list[0])
+    const ulMovies = document.querySelector("#movie-items")
+    const h4 = document.querySelector("h4")
+    ulMovies.innerHTML = ""
+    list.forEach(el => {
+        const li = document.createElement("li")
+        const hr = document.createElement("hr")
+        h4.textContent = `Ordered by: ${listString}`
+        li.textContent = el.title
+        li.className = "listcategories"
+        ulMovies.appendChild(li)
+        ulMovies.appendChild(hr)
+    })
+    viewMovieInfo()
+}
 
-// }
-// addDogBreeds()
+function viewMovieInfo () {
+    const ulMovies = document.querySelector("#movie-items")
+    const movieList = ulMovies.getElementsByTagName("li")
+    Array.from(movieList).forEach(movie => {
+        movie.addEventListener("click", event => {
+            fetch("https://mcuapi.herokuapp.com/api/v1/movies")
+            .then(data => data.json())
+            .then(json => {
+                const movieToFeature = json.data.find(el => el.title === movie.textContent)
+                displayMovieCenter(movieToFeature)
+            })
+        })
+    })
+}
 
-//this app will be for someone that is new to MCU, and wants to start on a journey to watch all the movies
-//and keep track of them
-//features will include:
+
+
+
+
 
 
 
